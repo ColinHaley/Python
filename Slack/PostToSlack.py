193a4cd64071f430a2bc670b00498957a41ac8cb
Colin Haley <admin@colinhaley.net>
2015-09-23 14:33:17 -0400

    typos

--- Slack/PostToSlack.py
+++ Slack/PostToSlack.py
@@ -26,12 +26,12 @@ __token__ = config.get('LogPoster','token')
 # User token from config file to generate 
 client = SlackClient(__token__)
 __channel__ = '#logging-scripts'
-__username__ = 'Trade Room Support Script Logger'
+__username__ = 'Script Logger'
 
 client.chat_post_message(__channel__, 'Update-ComputerStatistics; Start time: 2:03', username=__username__)
 time.sleep(2.5)
 client.chat_post_message(__channel__, 'Update-ComputerStatistics; End time: 2:05', username=__username__)
-__username__ = 'Trade Room Support SQL Logger'
+__username__ = 'SQL Job Logger'
 time.sleep(2.5)
 client.chat_post_message(__channel__, 'Repopulate CustomCMDB Table; Start time: 2:05', username=__username__)
 time.sleep(2.5)
@@ -39,7 +39,7 @@ client.chat_post_message(__channel__, 'Repopulate CustomCMDB Table; End time: 2:
 
 raw_input()
 __channel__ = '#logging-serverstats'
-__username__ = 'Trade Room Support Statistics Logger'
+__username__ = 'Statistics Logger'
 client.chat_post_message(__channel__, 'EUSAPPBAL500: [CPU]:20% - [RAM]:64% - [Tasks Running]:2', username=__username__)
 time.sleep(1)
 client.chat_post_message(__channel__, 'SQLTRSPBAL500: [CPU]:4% - [RAM]:15% - [Open Connections]:12', username=__username__)
