c53b890d4206859c22271982bfeb3cc78df8c122
Colin Haley <admin@colinhaley.net>
2015-09-22 23:53:23 -0400

    Add first attempt at /r/frugalmalefashion scraper

---
+++ RedditScraper.py
@@ -0,0 +1,36 @@
+"""
+__author__	= 'Colin Haley, aka Kazra, aka /u/Gemjump'
+__purpose__ 	= 'Watch a list of subreddits for a different list of keywords'
+
+# Other Resources
+
+
+# Mandatory External Libraries
+Praw:		https://gist.github.com/shrayasr/100005943
+
+"""
+
+try:
+    import praw
+except:
+    # Python 3 handling
+    from praw import praw
+
+r = praw.Reddit('Searcher for things I care about on Reddit; Made my /u/Gemjump; Run on Debian Wheezy')
+
+listOfSubreddits = ['frugalmalefashion']
+listOfKeyWords = ['Wolverine']
+
+for x in range(0,len(listOfSubreddits)):
+    
+    sub = r.get_subreddit(listOfSubreddits[x],fetch=True)
+
+    posts = sub.get_new()
+
+    while True:
+        try:
+            time.sleep(2.5)
+            post = next(posts)
+            for x in range(0,len(listOfKeyWords)):
+                if listOfKeyWords[x] in post:
+                    print post.id
