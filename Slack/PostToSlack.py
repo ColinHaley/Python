690cb34e956267788fdf286e21566a14423e6db4
Colin Haley <admin@colinhaley.net>
2015-09-23 11:16:52 -0400

    Update docstring

--- Slack/PostToSlack.py
+++ Slack/PostToSlack.py
@@ -1,1 +1,14 @@
-# Post to Slack script
+"""
+__author__ 	= 'Colin Haley'
+__purpose__ 	= 'Demo slack to team at work'
+
+##############
+#REQUIREMENTS#
+##############
+
+* pyslack
+	pip install pyslack-real
+* python 2.7.3 and above
+
+"""
+
