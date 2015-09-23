088622196d076fde33393321c9f8f203e5d71b74
Colin Haley <admin@colinhaley.net>
2015-09-23 11:54:01 -0400

    Config Parser reading

--- Slack/PostToSlack.py
+++ Slack/PostToSlack.py
@@ -8,7 +8,20 @@ __purpose__ 	= 'Demo slack to team at work'
 
 * pyslack
 	pip install pyslack-real
+* ConfigParser
 * python 2.7.3 and above
 
 """
+try:
+    import pyslack
+except:
+    # handle python 3 silly imports
+    from pyslack import pyslack
 
+# ConfigParser is a native module, no try except needed
+import ConfigParser
+
+# Get config information from ../Authentication/Slack.cfg
+
+config = ConfigParser.RawConfigParser()
+config.read('../Authentication/Slack.cfg')
