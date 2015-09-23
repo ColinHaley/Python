fcd6d289d0d8dd59b64e6790b6c2ca4e564a32f4
Colin Haley <admin@colinhaley.net>
2015-09-23 12:22:41 -0400

    v1 posting successfully without using OAuth

--- Slack/PostToSlack.py
+++ Slack/PostToSlack.py
@@ -12,16 +12,21 @@ __purpose__ 	= 'Demo slack to team at work'
 * python 2.7.3 and above
 
 """
-try:
-    import pyslack
-except:
-    # handle python 3 silly imports
-    from pyslack import pyslack
-
-# ConfigParser is a native module, no try except needed
+from pyslack import SlackClient
 import ConfigParser
 
 # Get config information from ../Authentication/Slack.cfg
 
 config = ConfigParser.RawConfigParser()
 config.read('../Authentication/Slack.cfg')
+
+__token__ = config.get('LogPoster','token')
+
+# User token from config file to generate 
+client = SlackClient(__token__)
+__channel__ = '#logging-scripts'
+__username__ = 'Colin'
+while True:
+    __message__ = raw_input()
+    client.chat_post_message(__channel__, __message__, username=__username__)
+
