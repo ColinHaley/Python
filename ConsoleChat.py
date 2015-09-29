3e6eff56905183b84997878372fae3c648f7c358
Colin Haley <admin@colinhaley.net>
2015-09-29 12:10:34 -0400

    Add basic authentication to console chat

---
+++ ConsoleChat.py
@@ -0,0 +1,31 @@
+"""
+I find bash scripts to be pretty gross.
+This script will replace the nasty direct send commands used right now
+with a more elegant script that enables commands, moderator chats, and direct messages.
+"""
+
+try:
+    import subprocess
+except:
+    from subprocess import subprocess
+
+try:
+    import ConfigParser
+except:
+    from ConfigParser import ConfigParser
+
+# Module level variables
+
+config = ConfigParser.RawConfigParser()
+config.read('../Authentication/ConsoleChat.cfg')
+
+__chatpass__	= config.get('ConsoleChat','Password')
+__username__	= raw_input('Who are you? ')
+
+# Validate admin by password confirmation
+
+__validated__ 	= False
+while __validated__ == False:
+    __enterpass__	= raw_input('Please enter the console chat password: ')
+    if __enterpass__ == __chatpass__:
+        __validated__ = True
