194a16da4beb893226b0788d204ca21e9e82c53f
Colin Haley <admin@colinhaley.net>
2015-09-30 11:35:42 -0400

    Quick update

--- ConsoleChat.py
+++ ConsoleChat.py
@@ -3,11 +3,7 @@ I find bash scripts to be pretty gross.
 This script will replace the nasty direct send commands used right now
 with a more elegant script that enables commands, moderator chats, and direct messages.
 """
-
-try:
-    import subprocess
-except:
-    from subprocess import subprocess
+from subprocess import call
 
 try:
     import ConfigParser
@@ -28,8 +24,15 @@ __username__	= raw_input('Who are you? ')
 
 # Validate admin by password confirmation
 
+def sendAllMessage(message):
+    preformatString = 'tellraw @a [\"\",{\"text\":\"<\"},{\"text\":\"Kazra\",\"color\":\"dark_red\"},{\"text\":\">: \",\"color\":\"none\"},{\"text\":\"{0}\",\"color\":\"none\"}]'.format(message)
+    call(["tmux","send","-t","vanilla",message,"ENTER"])
+
 __validated__ 	= False
 while __validated__ == False:
-    __enterpass__	= getpass.getpass('Please enter the console chat password: ')
+    __enterpass__ = getpass.getpass('Please enter the console chat password: ')
     if __enterpass__ == __chatpass__:
         __validated__ = True
+
+if __name__ == '__main__':
+    # Do stuff
